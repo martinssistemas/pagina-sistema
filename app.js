0c48b4a38a1af5896c0d982e1c8efcb5bbdd4a88
martinssistemas <140449255+martinssistemas@users.noreply.github.com>
2023-07-26 16:27:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-// Dados temporários de usuários registrados (substitua isso com a lógica do backend e banco de dados em um ambiente de produção real)
-var registeredUsers = [
-  { email: "usuario@exemplo.com", password: "senha123" },
-  // Adicione mais usuários aqui se necessário
-];
+// Buscar os usuários registrados do localStorage quando o script é carregado
+var registeredUsers = JSON.parse(localStorage.getItem('registeredUsers')) || [];
 
 function login() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
 
+  // Verificar se os campos estão preenchidos
   if (email.trim() === "" || password.trim() === "") {
     alert("Por favor, preencha o email e a senha.");
     return;
   }
 
-  // Verificar se as credenciais correspondem a uma conta registrada (substitua esta lógica com uma chamada ao backend para verificar as credenciais)
+  // Verificar se o email e senha correspondem a um usuário registrado
   var foundUser = registeredUsers.find(function(user) {
     return user.email === email && user.password === password;
   });
@@ -30,12 +28,13 @@ function register() {
   var email = document.getElementById("email").value;
   var password = document.getElementById("password").value;
 
+  // Verificar se os campos estão preenchidos
   if (email.trim() === "" || password.trim() === "") {
     alert("Por favor, preencha o email e a senha para criar uma conta.");
     return;
   }
 
-  // Verificar se o email já está registrado (substitua esta lógica com uma chamada ao backend para verificar o email)
+  // Verificar se o email já está registrado
   var isEmailRegistered = registeredUsers.some(function(user) {
     return user.email === email;
   });
@@ -43,10 +42,12 @@ function register() {
   if (isEmailRegistered) {
     alert("Este email já está registrado. Por favor, faça o login.");
   } else {
-    // Registrar o novo usuário (substitua esta lógica com a chamada ao backend para salvar os dados do novo usuário)
+    // Registrar o novo usuário e salvar no localStorage
     registeredUsers.push({ email: email, password: password });
+    localStorage.setItem('registeredUsers', JSON.stringify(registeredUsers));
     alert("Conta criada com sucesso! Faça o login para acessar a página principal.");
     // Redirecionar para a página de login após o registro ser concluído
     window.location.href = "index.html";
   }
 }
+
